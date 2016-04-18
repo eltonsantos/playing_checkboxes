a4d9ecb2375adb4b8832ec714faf8a84c78ad695
Elton Santos <elton@r2a.net.br>
2016-04-18 19:03:44 -0300

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Person.create name: "Elton", cpf: "11111111111"
+Person.create name: "Eloy", cpf: "22222222222"
+
+Employee.create person_id: 1, register: "01020304", salary: 2000
+Employee.create person_id: 1, register: "02030405", salary: 3000
+Employee.create person_id: 2, register: "03040506", salary: 4000
+
+Authorization.create employee_id: 1, contract_number: 11, total_value: 1000, parcel_value: 100, qtd_parcel: 10
+Authorization.create employee_id: 1, contract_number: 22, total_value: 2000, parcel_value: 200, qtd_parcel: 20
+Authorization.create employee_id: 2, contract_number: 33, total_value: 3000, parcel_value: 300, qtd_parcel: 30
+Authorization.create employee_id: 2, contract_number: 44, total_value: 4000, parcel_value: 400, qtd_parcel: 40
+Authorization.create employee_id: 3, contract_number: 55, total_value: 5000, parcel_value: 500, qtd_parcel: 50
