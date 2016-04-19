c008651cfaa6c36ee1ecdb62ec05fcb062e94e79
Elton Santos <elton@r2a.net.br>
2016-04-19 10:03:48 -0300

    Almost finished, finally!

--- app/controllers/refinancings_controller.rb
+++ app/controllers/refinancings_controller.rb
@@ -1,44 +1,27 @@
 class RefinancingsController < ApplicationController
 
-  # GET /refinancings
-  # GET /refinancings.json
   def index
     if params[:search_employee_by_cpf].present?
       @employees = Employee.search_cpf(params[:search_employee_by_cpf]).includes(:authorizations)
-      #@employees = Employee.search_cpf(params[:search_employee_by_cpf]).all
-      #@authorizations = Authorization.search_employee_by_cpf(params[:search_employee_by_cpf]).all
     else
       @authorizations = []
     end
   end
 
-  # GET /refinancings/1
-  # GET /refinancings/1.json
   def show
     @refinancing = Refinancing.find(params[:id])
   end
 
-  # GET /refinancings/new
   def new
     if params[:authorization].present?
       @selected_ids = params[:authorization][:contract_number]
       @authorizations = Authorization.where("contract_number in (?)", @selected_ids)
-      Authorization.where(id: params[:authorization][:id]).update_all(value_solve: params[:authorization][:value_solve], situation: 2)
+      Authorization.where(contract_number: params[:authorization][:contract_number]).update_all(value_solve: params[:authorization][:value_solve], situation: 2)
     end
     @employee = Employee.search_cpf(params[:search_employee_by_cpf])
     @refinancing = Refinancing.new
-    # @refinancing.build_authorization
-    #params[:authorization][:contract_ids].each_with_index do |index, id|
-    #  Authorization.find(id).update_column(value_solve: params[:authorization][:value_solve], situation: 2)
-    #end
-    #params[:authorization][:contract_ids].each do |id|
-    #  Authorization.find(id).update_column(value_solve: params[:authorization][:value_solve])
-    #end
-    #Authorization.where(id: params[:authorization]['contract_ids']).update_all(value_solve: params[:authorization]['value_solve'], situation: 2)
   end
 
-  # POST /refinancings
-  # POST /refinancings.json
   def create
     @refinancing = Refinancing.new(refinancing_params)
 
@@ -55,7 +38,6 @@ class RefinancingsController < ApplicationController
 
   private
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def refinancing_params
       params.require(:refinancing).permit(:employee_id, :authorization_id, :contract_number, :situation, :total_value, :parcel_value, :qtd_parcel, :refinancing_date, authorization_attributes: [ :value_solve ])
     end
