d3d399c2d9da23681563cf1909e73c6626b310ca
Elton Santos <elton@r2a.net.br>
2016-04-20 18:03:39 -0300

    Commiting something

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,5 +16,4 @@ Employee.create person_id: 2, register: "03040506", salary: 4000
 Authorization.create employee_id: 1, contract_number: 11, total_value: 1000, parcel_value: 100, qtd_parcel: 10
 Authorization.create employee_id: 1, contract_number: 22, total_value: 2000, parcel_value: 200, qtd_parcel: 20
 Authorization.create employee_id: 2, contract_number: 33, total_value: 3000, parcel_value: 300, qtd_parcel: 30
-Authorization.create employee_id: 2, contract_number: 44, total_value: 4000, parcel_value: 400, qtd_parcel: 40
 Authorization.create employee_id: 3, contract_number: 55, total_value: 5000, parcel_value: 500, qtd_parcel: 50
